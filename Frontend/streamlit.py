d75fbf585fb257c40bb60135e37bb51f6d11e705
Lennert Verbeeck <lennert.verbeeck@gmail.com>
2023-06-13 11:04:26 +0200

    functioning streamlit and a gitignore added

--- Frontend/streamlit.py
+++ Frontend/streamlit.py
@@ -1,42 +1,70 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import requests
 from PIL import Image
+import requests
+import io
+import pandas as pd
 
+# Set page tab display
+st.set_page_config(
+    page_title="Simple Image Uploader",
+    page_icon='🖼️',
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
-st.set_page_config(layout='wide')
-col1, col2 = st.columns(2)
+# API URL for vegetable recognition
+api_url = "https://veggideas-5s5h4zi4hq-ew.a.run.app"
 
-'''
-# TaxiFareModel front
-'''
-
-
+# App title and description
+st.header('Simple Image Uploader 📸')
 st.markdown('''
-Welcome to the veggideas website
-''')
-def main():
-    st.markdown('''
-        #Welcome to the veggideas website
-        ''')
-    # Set the title of the app
-    st.markdown("##Recipe generator")
+            > This is a Le Wagon boilerplate for any data science projects that involve exchanging images between a Python API and a simple web frontend.
 
-    # Upload image
-    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+            > **What's here:**
 
-    if uploaded_file is not None:
-        # Display the uploaded image
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+            > * [Streamlit](https://docs.streamlit.io/) on the frontend
+            > * [FastAPI](https://fastapi.tiangolo.com/) on the backend
+            > * [PIL/pillow](https://pillow.readthedocs.io/en/stable/) and [opencv-python](https://github.com/opencv/opencv-python) for working with images
+            > * Backend and frontend can be deployed with Docker
+            ''')
 
-        # Make predictions when the user clicks the button
-        if st.button('Make Predictions'):
-            # Call the function to make predictions using your API
-            predictions = "carrot"
+st.markdown("---")
 
-            # Display the predictions
-            st.subheader("Predictions:")
-            for i, prediction in enumerate(predictions):
-                st.write(f"{i+1}. {prediction}")
+### Create a native Streamlit file upload input
+st.markdown("### Let's do vegetable recognition 👇")
+img_file_buffer = st.file_uploader('Upload an image')
+
+if img_file_buffer is not None:
+    # Display the image uploaded by the user
+    image = Image.open(img_file_buffer)
+    st.image(image, caption="Here's the image you uploaded ☝️")
+
+    # Make predictions when the user clicks the button
+    if st.button('Make Predictions'):
+        with st.spinner("Wait for it..."):
+            # Send the image to the API endpoint
+            # convert the PIL image to byte array
+            image_bytes = io.BytesIO()
+            image.save(image_bytes, format='JPEG')
+            image_bytes = image_bytes.getvalue()
+
+            st.write("Sending image to the API...")
+
+            # Use 'rb' if you get an error about 'bytes-like object is required, not str'
+            api_url = api_url + "/predict"
+            response = requests.post(api_url, files={'img': image_bytes})
+            st.write(response.status_code)
+            if response.status_code == 200:
+                # Parse the predictions from the JSON response
+
+                data = response.json()
+
+                # Create a DataFrame from the predictions data
+                df = pd.DataFrame(data)
+
+                # Display the predictions as a table
+                st.subheader("Predictions:")
+                st.dataframe(df)
+
+            else:
+                st.error("Error making predictions. Please try again.")
