2399ca53886a68c5b3109fb34f1dd5a3e9608df6
Arthur Dercq <arthur.dercq@skynet.be>
2023-06-16 11:21:29 +0200

    made some changes for caps

--- Frontend/filter_df.py
+++ Frontend/filter_df.py
@@ -55,8 +55,7 @@ def filter_dataframe(df):
                     f"Which ingredient do you want to add",
                     )
                 if user_text_input:
-                    df = df[df[column].astype(str).str.contains(user_text_input)]
-
+                    df = df[df[column].astype(str).str.contains(user_text_input, case=False)]
 
 
         return df
